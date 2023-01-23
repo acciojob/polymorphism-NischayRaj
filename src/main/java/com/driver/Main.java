e07c0867cb41ae3972028d41871936f75cfa091e
Nischay <nischayraj11@gmail.com>
2023-01-23 12:42:55 +0530

    Checking tc

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+        public static void main(String[] args) {
+            Product p = new Product();
+            int result1 = p.product(5, 10); // calls the first product method
+            int result2 = p.product(5, 10, 15); // calls the second product method
+            double result3 = p.product(5.5, 10.5); // calls the third product method
+            System.out.println(result1);
+            System.out.println(result2);
+            System.out.println(result3);
+        }
+    }
 
-}
+    class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
